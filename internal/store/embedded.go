34252a4ab7efef753ff06825ac7da80232f2311a
Shivansh Kuchchal <shivanshbox@gmail.com>
2026-06-25 00:54:25 +0530

    feat: add missing database methods

--- internal/store/embedded.go
+++ internal/store/embedded.go
@@ -67,12 +67,69 @@ func (e *Embedded) BulkInsertValidationMessages(ctx context.Context, messages []
 
 // DeleteValidationMessages implements [Client].
 func (e *Embedded) DeleteValidationMessages(ctx context.Context, ids []int) error {
-	panic("unimplemented")
+	query, args := e.queryDeleteValidationMessages(ids)
+
+	// Execute query.
+	result, err := e.db.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("failed to execute query: %w", err)
+	}
+
+	aff, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("failed to get affected row count: %w", err)
+	}
+
+	// Verify that expected number of rows were deleted.
+	if int(aff) != len(ids) {
+		return fmt.Errorf("unexpected number of rows were deleted: %d, expected: %d", aff, len(ids))
+	}
+
+	return nil
 }
 
 // ListValidationMessages implements [Client].
 func (e *Embedded) ListValidationMessages(ctx context.Context, limit int) ([]ValidationMessageRow, error) {
-	panic("unimplemented")
+	query, args :=
+		`SELECT id, message, created_at FROM validations ORDER BY created_at LIMIT $1`,
+		[]any{limit}
+
+	// Execute query.
+	rows, err := e.db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("error in query execution: %w", err)
+	}
+	// Cleanup.
+	defer func() { _ = rows.Close() }()
+
+	// This will store the results.
+	var messages []ValidationMessageRow
+
+	// Loop over rows for decoding data.
+	for rows.Next() {
+		var m ValidationMessageRow
+		var textContent string
+
+		if err := rows.Scan(&m.ID, &textContent, &m.CreatedAt); err != nil {
+			return nil, fmt.Errorf("failed to scan row: %w", err)
+		}
+
+		// Convert from plain text into structure.
+		if err := json.Unmarshal([]byte(textContent), &m.Message); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal message content into struct: %w", err)
+		}
+
+		// Collect.
+		messages = append(messages, m)
+	}
+
+	// If there's an error while scanning (not returned by rows.Scan), rows.Next may return false
+	// and the loop may end early. This error is caught by rows.Err here.
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows.Err returned error: %w", err)
+	}
+
+	return messages, nil
 }
 
 // Close the database connection.
@@ -101,4 +158,17 @@ func (e *Embedded) queryBulkInsertValidationMessages(
 	// Remove trailing comma-space from the earlier string-building.
 	values = strings.TrimSuffix(values, ", ")
 	return `INSERT INTO validations (message) VALUES ` + values + ";", args, nil
+}
+
+func (e *Embedded) queryDeleteValidationMessages(ids []int) (string, []any) {
+	args := make([]any, len(ids))
+	arrString := "("
+	for i := range ids {
+		args[i] = ids[i]
+		arrString += fmt.Sprintf(`$%d, `, i+1)
+	}
+	arrString = strings.TrimSuffix(arrString, ", ")
+	arrString += ")"
+
+	return `DELETE FROM validations WHERE id IN ` + arrString, args
 }
--- internal/store/types.go
+++ internal/store/types.go
@@ -1,6 +1,10 @@
 package store
 
 import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+
 	"github.com/shivanshkc/observer/pkg/rippled"
 )
 
@@ -8,5 +12,28 @@ import (
 type ValidationMessageRow struct {
 	ID        int
 	Message   rippled.MessageValidationReceived
-	CreatedAt int64
+	CreatedAt Timestamp
+}
+
+// Timestamp represents an epoch timestamp that gets stored in the database in TIMESTAMP format.
+type Timestamp int64
+
+func (t *Timestamp) Scan(value any) error {
+	switch asserted := value.(type) {
+	case time.Time:
+		*t = Timestamp(asserted.UnixMilli())
+	case int64:
+		*t = Timestamp(asserted)
+	default:
+		return fmt.Errorf("unknown type: %T", asserted)
+	}
+	return nil
+}
+
+func (t Timestamp) Value() (driver.Value, error) {
+	return time.UnixMilli(int64(t)), nil
+}
+
+func (t Timestamp) ToTime() time.Time {
+	return time.UnixMilli(int64(t))
 }
